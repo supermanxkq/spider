9f2b777ef50c1644338babb2762b614bd88b2aa7
supermanxkq <994028591@qq.com>
2017-09-09 14:34:23 +0800

    update sql

---
+++ SQL/update.sql
@@ -0,0 +1,6 @@
+update user   set realName='王中堃' where mobile='15063016088';
+update user   set coNa='济宁市任城区米蘭春天布艺生活馆' where mobile='15588732166';
+update user   set address='山东省昌邑市岞山镇皂角树村127号' where  mobile='18663991856';
+update user   set coNa='聊城市东昌府区崑豪男装批发部'  where mobile='15552182186';
+update user set address='定陶县冉堌镇冉中路4号（冉堌工业园）', coAddress='定陶县冉堌工业园冉中路4号' where mobile='15163041888';
+update user set  address='济宁市任城区米蘭春天布艺生活馆' where mobile='18053721777';
